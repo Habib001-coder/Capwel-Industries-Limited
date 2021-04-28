00e6ed9ada7754d077feb0198e72c78aefa6d3fa
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-04-28 15:56:12 +0300

    add new files and modifying other content

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
-  });
+// $(document).ready(function(){
+//     $("#work1").mouseover(function(){
+//       $("#overlay").show();
+//     }).mouseout(function(){
+//       $("#overlay").hide();
+//     });
+//   });
+//   $(document).ready(function(){
+//     $("#work2").mouseover(function(){
+//       $("#overlay2").show();
+//     }).mouseout(function(){
+//       $("#overlay2").hide();
+//     });
+//   });
+//   $(document).ready(function(){
+//     $("#work3").mouseover(function(){
+//       $("#overlay3").show();
+//     }).mouseout(function(){
+//       $("#overlay3").hide();
+//     });
+//   });
 
 
   
