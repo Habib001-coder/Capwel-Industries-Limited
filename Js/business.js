552fb5e756b9a5bc17f40180a2cef7cca9182057
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool>
2021-04-28 06:31:30 +0300

    given submit button a function

--- Js/business.js
+++ Js/business.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
-      var fistName = $document.getElementById("fname")
-      var lastName = $document.getElementById("lname")
-      var email = $document.getElementById("email")
-      var number = $document.getElementById("number")
-      if (firstName === "" && lastName === "" &&  email === ""){
-        alert("please insert your credentials")
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
-      
-    });
- 
+  $("#form1").submit(function(event){
+   var firstName = $("input#fname").val();
+   var lastName = $("input#lname").val();
+   var email = $("input#email").val();
+   var phoneNumber = $("input#number").val();
+   var message = $("input#message").val();
+   if (firstName === "" && email === "" && message === ""){
+      alert("please insert your credentials")
+   }
+   else
+   if ( firstName && lastName )
+   alert(firstName + ", of " + email + ", we have receive your massage, you will be contacted shortly. Thanks for reaching out")
+   else
+   if (firstName === "") {
+       alert("Please Insert your name")
+   } else
+   if (email === "") {
+       alert("Enter your Email")
+   } else
+   if (message === "") {
+       alert("Please insert your message!")
+   }
+})
+})
